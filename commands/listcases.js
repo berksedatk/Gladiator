74cfcdf49c423f66319daabe187bf6ce1451d379
SaxDev <30241194+SaxDev@users.noreply.github.com>
2020-06-01 02:22:41 +0300

    Update listcases.js

--- commands/listcases.js
+++ commands/listcases.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const Guild = require("../schemas/guild.js");
 const mongoose = require("mongoose");
+const prettyms = require("pretty-ms");
 
 module.exports = {
   name: "listcases",
@@ -31,7 +32,10 @@ module.exports = {
         const keys = guild.cases.keys()
         guild.cases.forEach((casee, key) => {
           if (casee.user.id === user.id) {
-            list.push(`\`Case #${key}\` \n**Reason:** ${casee.reason} \n**Date:** ${new Date(casee.time)} \n**By:** ${casee.by.tag} \n**Action:** ${casee.action} \n`)
+            let listmsg = `**Issued User:** ${cases.user.tag}(${cases.user.id}) \n**Issued By**: <@${cases.by.id}>(${cases.by.id}) \n**Action:** ${cases.action} \n**Reason:** ${cases.reason}`
+          
+            cases.action === "mute" ? (cases.length === null ? listmsg += `\n**Muted for:** No time was provided.` : listmsg += `\n**Muted for:** ${prettyms(cases.length, {verbose: true})}`) : false
+            list.push({ title: `**Case ID:** #${key}`, field: listmsg})            
           }
         })
         
@@ -48,21 +52,22 @@ module.exports = {
           if (args[1]) {
             if (args[1] > pages + 1) return message.channel.send(":x: | This page does not exist.")
             for (let i = goto; i < goto + 10; i++) {
-              embedlist.push(list[i])
+              casesEmbed.addField(list[i].title, list[i].field)
             }
             casesEmbed.setFooter(`Page of ${args[1]}/${pages + 1} - Requested by ${message.author.tag}`, message.author.avatarURL())
           } else {
             for (let i = 0; i < 10; i++) {
-              embedlist.push(list[i])            
+              casesEmbed.addField(list[i].title, list[i].field)            
             }
             casesEmbed.setFooter(`Page of 1/${pages + 1} - Requested by ${message.author.tag}`, message.author.avatarURL())
           }
         } else {
-          embedlist = list[0] ? list : "- No recorded cases."
+          embedlist = list[0] ? list : "- No recorded cases."          
           casesEmbed.setFooter(`Page of 1/1 - Requested by ${message.author.tag}`, message.author.avatarURL())
         }
         
-        casesEmbed.setDescription(embedlist)
+        list < 1 ? casesEmbed.setDescription(" - No recorded cases.") : list.forEach(Case => casesEmbed.addField(Case.title, Case.field))
+        casesEmbed.setFooter(`Page of 1/1 - Requested by ${message.author.tag}`, message.author.avatarURL())
         message.channel.send(casesEmbed)
       }
     })
