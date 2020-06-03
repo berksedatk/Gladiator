7efa4ddb4b1c78c55982944563ddb706b8a25cb3
SaxDev <30241194+SaxDev@users.noreply.github.com>
2020-06-03 23:04:31 +0300

    Update events.js

--- util/handlers/events.js
+++ util/handlers/events.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-module.exports = (bot, dbl, db) => {
+module.exports = (bot) => {
   const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
   for (let i = 0; i < eventFiles.length; i++) {
     const event = require(`../../events/${eventFiles[i]}`);
     const name = eventFiles[i].split(".")[0];
     bot.events.set(name, event);
-    bot.on(name, (...args) => bot.events.get(name).execute(bot, ...args, db, dbl));
+    bot.on(name, (...args) => bot.events.get(name).execute(bot, ...args));
   }
 }
