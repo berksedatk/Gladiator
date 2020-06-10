871a74a3f2688d2647fbd78b1567093e30d6bf9b
SaxDev <30241194+SaxDev@users.noreply.github.com>
2020-06-10 19:24:59 +0300

    Update userinfo.js
    User will be defined as author when no arguments provided.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,27 +9,34 @@ module.exports = {
   cooldown: 5,
   guildOnly: true,
   async execute(bot, message, args) {
+    
+    let user;
 
-    if (!args[0]) return message.channel.send(":x: | You didn't provide a user.");
-    let user = message.mentions.users.first() ? message.mentions.users.first()
-      : (message.guild.members.cache.get(args[0]) ? message.guild.members.cache.get(args[0])
-      : (message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).size > 0 ? message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).array()
-      : undefined))
-    if (!user) return message.channel.send(":x: | You didn't provide a true user.");
-
-    if (user.length > 1) {
-      let usermsg = "";
-        for (let i = 0; i < (user.length > 10 ? 10 : user.length); i++) {
-      usermsg += `\n${i + 1} -> ${user[i].user.username}`;
-      }
-
-      let msg = await message.channel.send("", {embed: {description: `**There are multiple users found with name '${args[0]}', which one would you like to use?** \n${usermsg}`, footer: {text: "You have 30 seconds to respond."}, timestamp: Date.now()}});
-      let collected = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 30000, errors: ['time'] })
-      if (Number(collected.first().content) > user.length) return message.channel.send(":x: | Invalid user number. Command cancelled.");
-      user = user[collected.first().content - 1]
-      msg.delete()
+    if (!args[0]) {
+      user = message.member
     } else {
-      user = user[0] || message.guild.members.cache.get(user.id)
+
+      user = message.mentions.users.first() ? message.mentions.users.first()
+        : (message.guild.members.cache.get(args[0]) ? message.guild.members.cache.get(args[0])
+        : (message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).size > 0 ? message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).array()
+        : undefined))
+
+      if (!user) return message.channel.send(":x: | You didn't provide a true user.");
+
+      if (user.length > 1) {
+        let usermsg = "";
+          for (let i = 0; i < (user.length > 10 ? 10 : user.length); i++) {
+        usermsg += `\n${i + 1} -> ${user[i].user.username}`;
+        }
+
+        let msg = await message.channel.send("", {embed: {description: `**There are multiple users found with name '${args[0]}', which one would you like to use?** \n${usermsg}`, footer: {text: "You have 30 seconds to respond."}, timestamp: Date.now()}});
+        let collected = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 30000, errors: ['time'] })
+        if (Number(collected.first().content) > user.length) return message.channel.send(":x: | Invalid user number. Command cancelled.");
+        user = user[collected.first().content - 1]
+        msg.delete()
+      } else {
+        user = user[0] || message.guild.members.cache.get(user.id)
+      }
     }
 
     let presenceMes;
