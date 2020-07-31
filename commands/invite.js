a60258168130ff80301edf5fadd8dd1f7dfe8e4c
SaxDev <30241194+SaxDev@users.noreply.github.com>
2020-07-31 11:56:58 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Invites for support channel and bot auth.",
   execute(bot, message, args) {
     const inviteEmbed = new MessageEmbed()
-    .setDescription("To invite Gladiator to your server click [here](https://gladiatorbot.glitch.me/inviterec). \n\nJoin the support server Sax's Bot Dump by clicking [here](https://discord.gg/PqjP3Dy)")
+    .setDescription("To invite Gladiator to your server click [here](https://gladiatorbot.ga/invite). \n\nJoin the support server Sax's Bot Dump by clicking [here](https://gladiatorbot.ga/support)")
     .setColor("GOLD")
     .setThumbnail(bot.user.avatarURL())
     .setFooter("Gladiator by Sax#6211")
