f0a5a1571b72fd4cdd9191e89f80e99590555ed5
SaxDev <berksedat123@gmail.com>
2020-09-19 14:04:37 +0300

    Removed unnecessary files

--- util/handlers/commands.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-module.exports = (bot) => {
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-      const command = require(`../../commands/${file}`);
-      bot.commands.set(command.name, command);
-    }
-}
