c3489195ab8455c97743be8aceea3b91726fe3e6
Berk S <30241194+SaxDev@users.noreply.github.com>
2021-12-13 12:33:58 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -5,10 +5,10 @@ module.exports = {
   description: "Invite Gladiator to your server or join the support server!",
   execute(bot, message, args) {
     const inviteEmbed = new MessageEmbed()
-    .setDescription("To invite Gladiator to your server click [here](https://gladiatorbot.ga/invite). \n\nJoin the support server by clicking [here](https://discord.gg/JQZquyU)")
+    .setDescription("To invite Gladiator to your server click [here](https://discord.com/api/oauth2/authorize?client_id=507604696609849355&permissions=431040621687&scope=bot%20applications.commands). \n\nJoin the support server by clicking [here](https://discord.gg/JQZquyU)")
     .setColor("GOLD")
     .setThumbnail(bot.user.avatarURL())
-    .setFooter("Gladiator by Sax#6211")
+    .setFooter("Gladiator by Sax#6211 and noodle#8720")
     .setTimestamp()
     message.channel.send(inviteEmbed)
   }
