5017c94eb58cc2e3e538901404f3e6622ea4ae8f
Berk S <30241194+SaxDev@users.noreply.github.com>
2022-12-18 00:31:59 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,59 +5,7 @@ const Mute = require("../schemas/mute.js");
 const Guild = require('../schemas/guild.js');
 
 module.exports = async client => {
-
-  //DBL
-  const DBL = require('dblapi.js');
-  const dbl = new DBL(process.env.DBL_TOKEN, { webhookPort: 3000, webhookAuth: 'homework' });
-
-  //Stats
-  dbl.postStats(client.guilds.cache.size)
-
-  //Voting
-  dbl.webhook.on('ready', hook => {
-    console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
-  });
-
-  dbl.webhook.on('vote', vote => {
-    const user = client.users.cache.get(vote.user)
-    if (user) {
-      client.channels.cache.get("673867340810551342").send({
-        embed: {
-          author: {
-            name: user.tag,
-            icon: user.avatarURL()
-          },
-          description: "Just voted for the bot!",
-          footer: {
-            text: "Vote by using g!vote command."
-          },
-          color: "GOLD",
-          timestamp: Date.now()
-        }
-      })
-      user.send("Thanks for voting! You can vote again in 12 hours!").then(() => {
-        console.log(`ready: Voting - ${user.tag} voted for the bot!`)
-      }).catch(err => {
-        console.log(`ready: Voting - ${user.tag} voted for the bot! But could not be messaged.`)
-      })
-    } else {
-      client.channels.cache.get("673867340810551342").send({
-        embed: {
-          author: {
-            name: vote.user,
-          },
-          description: "Just voted for the bot!",
-          footer: {
-            text: "Vote by using g!vote command."
-          },
-          color: "GOLD",
-          timestamp: Date.now()
-        }
-      })
-      console.log(`ready: Voting - ${user.tag} voted for the bot! But could not find it user cache.`)
-    }
-  });
-
+  
   //User activity
   client.user.setActivity("Google Chrome | g!help", {
     type: "PLAYING"
